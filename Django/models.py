000bcf8ae82004c99c9b953885efb79366336577
Fernando <fernan.hdez2015@gmail.com>
2022-10-27 22:40:34 -0400

    more

---
+++ Django/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.db import connection
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+
+# Create your models here.
+
+
+class Farm(models.Model):
+    name = models.CharField("Nombre", max_length=50)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE) # Is auto_created the user who create the table. Need modify the method in the 
+    # view form_valid
+    location = models.PointField("Localizacion", srid=4326,blank=False,null=False)
+    date_created = models.DateTimeField("Fecha de creado", auto_now_add=True) # Is auto_created the datetime when is created the table.
+    unitary_price = models.FloatField("Precio Unitario")
+    source = models.ForeignKey("Source", on_delete=models.CASCADE, default=None) # The Entity Source betwing "" is for model declared after the 
+    # present model, that mean below.
+    parameter = models.ManyToManyField(Parameter) # Relationship Many To Many. The field is puting in only class model. Automaticaly 
+    # is created a new table with the two pk from the relantionship.
+
+    class Meta: # Model metadata is "anything that's not a field"
+        verbose_name = "Granja" # Show the name friendly to user in the admin template.
+
+
+    def __str__(self): # Show the srt name of objet, need return a string. 
+        return self.name
+
+
+
+
+    
+
+
+
+
+
+
--- Django/views.py
+++ Django/views.py
@@ -17,7 +17,7 @@ from SAF.forms import FarmForm
 @login_required(login_url="/login/")
 def index(request,*args, **kwargs):
     model = Farm
-    form_class = FarmForm
+    form_class = FarmForm # Is defined to use a form_model in the view. You must still specify the model. Example FarmForm
     initial = {'key': 'value'}
     context = {'segment': 'index'}
     farm_list = Farm.objects.all()[:20]
@@ -62,9 +62,53 @@ class FarmDetailView(DetailView):
 
 
 # CreateView use myapp/name_entity_form.html or use template_name suffix
-class FarmCreateView(CreateView):
+class FarmCreateView(LoginRequiredMixin, CreateView): # LoginRequiredMixin The user need to bee login.
     model = Farm
     fields = ['name']
+
+
+    def get_context_data(self, **kwargs):
+        initial = {'key': 'value'}
+        form_class = FarmForm
+        farm_list = Farm.objects.all()[:20]
+        porciento_con = 0
+        count_farm = Farm.objects.count()
+        last_farm = Farm.objects.filter(created_by=self.request.user).order_by("-id")
+        planSAF_farm = PlanSAF.objects.filter(created_by=self.request.user)
+        list_pk_farm_tmp = []
+        list_pk_farm = []
+        for safFarm in planSAF_farm:
+            list_pk_farm_tmp.append(safFarm.farm)
+        for item in list_pk_farm_tmp:
+            if item not in list_pk_farm:
+                list_pk_farm.append(item)
+        if count_farm > 0:
+            porciento_con = (len(list_pk_farm) * 100) / count_farm
+        form = form_class(initial=initial)
+        context={'form': form,
+         'farm_list': farm_list,
+         'count_farm': count_farm,
+         'last_farm': last_farm.count(),
+         'porciento_con': porciento_con,
+         'count_farm_planSAF': len(list_pk_farm),'segment': 'index'}
+        return context
+
+    def get_success_url(self):
+        if self.request.method == 'POST' and "_continue" in self.request.POST:
+            return reverse('home', kwargs={'pk': self.object.id})
+        else:
+            return reverse('home')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user # To assign the user loged in instance of form.
+
+        return super().form_valid(form)
+    self.request.user # The user loged
+
+    def form_valid(self, form): # 
+        form.instance.created_by = self.request.user # To auto insert the user that create the table
+
+        return super().form_valid(form)    
 
 
 # UpdateView use myapp/name_entity_form.html or use template_name suffix
